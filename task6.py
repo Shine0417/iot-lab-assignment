56e502c394a053506bcf4672aab3e958938e9ba4
B07902109 張翔文 <45307259+Shine0417@users.noreply.github.com>
2020-06-26 21:23:59 +0800

    Delete task6.py

--- task6.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node
-import numpy as np
-from autoware_auto_msgs.msg import BoundingBoxArray
-from autoware_auto_msgs.msg import RawControlCommand
-from nav_msgs.msg import Odometry
-
-def start():
-    return throttle, brake, front_steer
